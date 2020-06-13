27dd47c58376f737fd5e606ace8a6c64544d7e89
Trustverse-BrianNam <55431227+Trustverse-BrianNam@users.noreply.github.com>
2020-06-13 16:01:08 +0900

    Brian

--- app/src/main/java/com/example/howlstagram/MainActivity.kt
+++ app/src/main/java/com/example/howlstagram/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.howlstagram
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivYTZXVHG0I22ity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener
+{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_navigation.setOnNavigationItemReselectedListenerthis)
     }
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+CCCQ                R.id.action_home->{
+                    return true
+                }
+            }
+        }
 }
